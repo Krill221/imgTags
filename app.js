025e9bc47a548dc2ffd42786d5f4cfe4197ede44
Еропкин Кирилл <krill2211@yandex.ru>
2022-11-17 02:19:54 +0300

    commit 4

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.use(cors(corsOptions))
 app.use(express.json({ type: ['application/json', 'application/csp-report'] }))
 app.use(express.static('public'))
 
+app.get('/ping', async (req, res) => {
+    res.send('pong')
+})
+
 app.post('/', async (req, res) => {
 
     //const imgBase64 = req.body.params.img
